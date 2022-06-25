19c9c02ff28e91fdebf602982e15bf8de998a76e
Miki Yutani <mkyutani@gmail.com>
2022-06-25 22:01:05 +0900

    Cannot find any links in news of the new spec of digital.go.jp

--- update.py
+++ update.py
@@ -9,9 +9,7 @@ import sys
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-verbose = False
-
-def get(url, url_prefix, encoding):
+def get(url):
 
     today = datetime.date.today().strftime('%Y/%m/%d')
 
@@ -23,7 +21,8 @@ def get(url, url_prefix, encoding):
         res = requests.get(url, headers=headers)
     except Exception as e:
         print(f'Failed to fetch {url}', file=sys.stderr)
-        return []
+        print(f'Exception {e}', file=sys.stderr)
+        return None
 
     if res.status_code == 403:
         try:
@@ -31,10 +30,10 @@ def get(url, url_prefix, encoding):
             res = requests.get(link, headers=headers)
         except Exception as e:
             print(f'Failed to fetch {url} ({res.status_code})', file=sys.stderr)
-            return []
+            return None
     elif res.status_code >= 400:
             print(f'Failed to fetch {url} ({res.status_code})', file=sys.stderr)
-            return []
+            return None
 
     bs = BeautifulSoup(res.content, 'html.parser')
     for t in bs.find_all('a'):
@@ -53,20 +52,27 @@ def get(url, url_prefix, encoding):
                 pass
             elif ref_lower.startswith('javascript:'):
                 pass
-            elif ref.startswith(url_prefix) is False:
-                pass
             else:
                 ref = urljoin(url, ref)
-                cs = t.strings
-                if cs is None:
-                    title = ''
+                if ref.startswith(url) is False:
+                    pass
+                elif ref == url:
+                    pass
                 else:
-                    title = re.sub('[　 ]+', ' ', '::'.join(filter(lambda x: len(x) > 0 and x != 'ニュース' and re.fullmatch('[0-9]+年[0-9]+月[0-9]+日', x) is None, [s.strip() for s in cs])))
+                    cs = t.strings
+                    if cs is None:
+                        title = ''
+                    else:
+                        title = re.sub('[　 ]+', ' ', '::'.join(filter(lambda x: len(x) > 0 and x != 'ニュース' and re.fullmatch('[0-9]+年[0-9]+月[0-9]+日', x) is None, [s.strip() for s in cs])))
 
-                links.append([today, ref, title])
-                if verbose:
-                    print(f'Found {ref}: {title}', file=sys.stderr)
+                    if len(title) == 0: # jump to last page
+                        pass
+                    elif re.fullmatch('[0-9]+', title): # jump to page number
+                        pass
+                    else:
+                        links.append([today, ref, title])
 
+    print(f'Remote: Found {len(links)} links', file=sys.stderr)
     return links
 
 def search(links, data):
@@ -76,40 +82,44 @@ def search(links, data):
     for l in links:
         for d in data:
             if l[1] == d[1] or l[2] == d[2]:
-                if verbose:
-                    print(f'Skip {l[1]}', file=sys.stderr)
                 break
         else:
             for r in result:
                 if l[1] == r[1] or l[2] == r[2]:
-                    if verbose:
-                        print(f'Skip {l[1]}', file=sys.stderr)
                     break
             else:
-                print(f'Register {l}', file=sys.stderr)
+                print(f'Remote: Registering {l}', file=sys.stderr)
                 result.append(l)
 
+    print(f'Remote: Found {len(result)} new links', file=sys.stderr)
     return result
 
-def update(url, url_prefix, csv_path, url_encoding, csv_encoding):
+def update(url, csv_path, csv_encoding):
+
+    print(f'-------- {csv_path} --------', file=sys.stderr)
 
     with open(csv_path, encoding=csv_encoding) as fd:
         reader = csv.reader(fd)
         data = [row for row in reader]
-        print(f'Found {len(data)} records', file=sys.stderr)
+        print(f'{csv_path}: Found {len(data)} records', file=sys.stderr)
 
-    new_data = search(get(url, url_prefix, url_encoding), data)
+    links = get(url)
+    if links is None:
+        return 1
 
-    shutil.copy2(f'{csv_path}', f'{csv_path}.new')
-    with open(f'{csv_path}.new', 'a') as fd:
-        writer = csv.writer(fd, lineterminator='\n')
-        writer.writerows(new_data)
-        print(f'Extend to {len(data) + len(new_data)} records', file=sys.stderr)
+    new_data = search(links, data)
+    if len(new_data) > 0:
+        shutil.copy2(f'{csv_path}', f'{csv_path}.new')
+        with open(f'{csv_path}.new', 'a') as fd:
+            writer = csv.writer(fd, lineterminator='\n')
+            writer.writerows(new_data)
+            print(f'{csv_path}: Stored new {len(new_data)} records', file=sys.stderr)
+        shutil.move(f'{csv_path}.new', f'{csv_path}')
 
     return 0
 
 if __name__ == '__main__':
     x = 0
-    x = x + update('https://www.digital.go.jp/councils/', 'https://www.digital.go.jp/councils/', 'jp-go-digital-news-meeting.csv', 'utf-8', 'utf-8')
-    x = x + update('https://www.digital.go.jp/news/', 'https://www.digital.go.jp/posts/', 'jp-go-digital-news-news.csv', 'utf-8', 'utf-8')
+    x = x + update('https://www.digital.go.jp/councils/', 'jp-go-digital-news-meeting.csv', 'utf-8')
+    x = x + update('https://www.digital.go.jp/news/', 'jp-go-digital-news-news.csv', 'utf-8')
     exit(x)
