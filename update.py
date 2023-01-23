0ad89e5cf265a230cad0e01d2c1c6235a3a8dac0
Miki Yutani <mkyutani@gmail.com>
2023-01-23 09:30:28 +0900

    #5, #6

--- update.py
+++ update.py
@@ -2,6 +2,7 @@
 
 import csv
 import datetime
+import json
 import pathlib
 import os
 import re
@@ -11,8 +12,9 @@ import sys
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-def get(url, site_group_lower=[]):
+def get(url, news=False, site_group_lower=[]):
 
+    url_lower = url.lower()
     today = datetime.date.today().strftime('%Y/%m/%d')
 
     links = []
@@ -39,6 +41,15 @@ def get(url, site_group_lower=[]):
 
     bs = BeautifulSoup(res.content, 'html.parser')
     for t in bs.find_all('a'):
+
+        section = t.find_parent('section')
+        if section:
+            cls = section.get('class')
+        else:
+            cls = None
+        if (news is False and section is not None) or (news is True and section is None):
+            continue
+
         ref = t.get('href')
         if ref is not None:
             ref = ''.join(filter(lambda c: c >= ' ', ref))
@@ -56,15 +67,13 @@ def get(url, site_group_lower=[]):
                 pass
             elif ref_lower.startswith('javascript:'):
                 pass
-            elif len(site_group_lower) > 0:
+            elif len(site_group_lower) > 0 and len(list(filter(lambda x: x.name == 'li', t.parents))) > 0:
                 for site_lower in site_group_lower:
                     if ref_lower.startswith(site_lower):
                         title = '::'.join([s.strip() for s in t.strings])
             elif len(t.find_all('h3')) > 0:
                 cs = t.strings
-                if cs is None:
-                    title = ''
-                else:
+                if cs:
                     title = re.sub('[　 ]+', ' ', '::'.join(filter(lambda x: len(x) > 0 and x != 'ニュース' and re.fullmatch('[0-9]+年[0-9]+月[0-9]+日', x) is None, [s.strip() for s in cs])))
 
             if title is None or len(title) == 0: # jump to last page
@@ -73,7 +82,17 @@ def get(url, site_group_lower=[]):
                 pass
             else:
                 ref_full = urljoin(url, ref)
-                links.append([today, ref_full, title])
+
+                dt = today
+                tm = t.find('time')
+                if tm:
+                    dt = ''.join(tm.strings)
+                    if dt:
+                        dt = dt.replace('年', '/')
+                        dt = dt.replace('月', '/')
+                        dt = dt.replace('日', '')
+
+                links.append([dt, ref_full, title])
 
     print(f'Remote: Found {len(links)} links', file=sys.stderr)
     return links
@@ -84,11 +103,11 @@ def search(links, data):
 
     for l in links:
         for d in data:
-            if l[1] == d[1] or l[2] == d[2]:
+            if l[1] == d[1] and l[2] == d[2]:
                 break
         else:
             for r in result:
-                if l[1] == r[1] or l[2] == r[2]:
+                if l[1] == r[1] and l[2] == r[2]:
                     break
             else:
                 print(f'Remote: Registering {l}', file=sys.stderr)
@@ -97,13 +116,13 @@ def search(links, data):
     print(f'Remote: Found {len(result)} new links', file=sys.stderr)
     return result
 
-def update(url, csv_path, csv_encoding, site_group=[]):
+def update(url, csv_path, csv_encoding, news=False, site_group=[]):
 
     site_group_lower = []
     for site in site_group:
         site_group_lower.append(site.lower())
 
-    print(f'-------- {csv_path} --------', file=sys.stderr)
+    print(f'-------- {url} --------', file=sys.stderr)
 
     data = []
     if os.path.isfile(csv_path):
@@ -115,34 +134,52 @@ def update(url, csv_path, csv_encoding, site_group=[]):
         pathlib.Path(csv_path).touch()
         print(f'{csv_path}: New file', file=sys.stderr)
 
-    links = get(url, site_group_lower=site_group_lower)
+    links = get(url, news=news, site_group_lower=site_group_lower)
     if links is None:
-        return 1
+        return -1
 
     new_data = search(links, data)
-    if len(new_data) > 0:
+    updated = len(new_data)
+    if updated > 0:
         shutil.copy2(f'{csv_path}', f'{csv_path}.new')
         with open(f'{csv_path}.new', 'a') as fd:
             writer = csv.writer(fd, lineterminator='\n')
             writer.writerows(new_data)
-            print(f'{csv_path}: Stored new {len(new_data)} records', file=sys.stderr)
+            print(f'{csv_path}: Stored new {updated} records', file=sys.stderr)
         shutil.move(f'{csv_path}.new', f'{csv_path}')
 
-    return 0
+    return updated
 
 if __name__ == '__main__':
-    x_all = 0
-    x_all = x_all + update('https://www.digital.go.jp/councils/', 'jp-go-digital-news-meeting.csv', 'utf-8')
-    x_all = x_all + update('https://www.digital.go.jp/news/', 'jp-go-digital-news-news.csv', 'utf-8')
-    x_prev = -1
-    x_prev2 = -1
-    for i in range(2, 20):
-        x = update('https://www.digital.go.jp/news/' + str(i) + '/', 'jp-go-digital-news-news.csv', 'utf-8')
-        x_prev2 = x_prev
-        x_prev = x
-        x_all = x_all + x
-        if x_prev2 == 0 and x_prev == 0:
-            break
-    x_all = x_all + update('https://www.digital.go.jp/procurement/', 'jp-go-digital-news-procurement.csv', 'utf-8',
-                           site_group=['https://www.p-portal.go.jp/'])
-    exit(x)
+
+    config_file_path = './jp-go-digital-news-config.json'
+    with open(config_file_path) as fconfig:
+        config = json.loads(fconfig.read())
+
+    result = 0
+
+    for site in config['sites']:
+        url = site['url']
+        report = site['report']
+        encoding = site['encoding']
+        news = site['news']
+        site_group = site['sitegroup']
+
+        x = update(url, report, encoding, news=news, site_group=site_group)
+        if x < 0:
+            result = 1
+
+        if news is True:
+            x_prev2 = 0
+            x_prev = 0
+            for i in range(2, 100):
+                x = update(url + str(i) + '/', report, encoding, news=news, site_group=site_group)
+                if x < 0:
+                    result = 1
+                    break
+                x_prev2 = x_prev
+                x_prev = x
+                if x_prev2 == 0 and x_prev == 0:
+                    break
+
+    exit(result)
